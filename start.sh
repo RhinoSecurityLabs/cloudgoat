9221ba7ffe9c0c9a24ae24f27d8a65b7ab44f37f
Joe Dickens <dickens.ja@gmail.com>
2018-07-20 15:49:06 -0700

    pre-flight commit

--- start.sh
+++ start.sh
@@ -2,9 +2,11 @@
 
 mkdir keys
 ssh-keygen -b 2048 -t rsa -f ./keys/cloudgoat_key -q -N ""
-
 awk 'BEGIN{getline k < "keys/cloudgoat_key.pub"}/insert_cloudgoat_key/{gsub("insert_cloudgoat_key",k)}1' terraform/ec2.tf > ./temp && mv ./temp terraform/ec2.tf
 
-cd terraform
-terraform plan -out plan.tf
-terraform apply -auto-approve plan.tf
+cd keys && gpg --batch --gen-key pgp_options
+
+cd ../terraform && terraform plan -out plan.tfout
+#terraform apply -auto-approve plan.tfout
+
+#./extract_creds.py
