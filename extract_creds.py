1886f727bf442e5a1f64dd99a0a60822089a5c11
Joe Dickens <dickens.ja@gmail.com>
2018-07-21 12:07:39 -0700

    intermediate commit

--- extract_creds.py
+++ extract_creds.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
 import json
-import gnupg
+import subprocess
 
 with open('./terraform/terraform.tfstate') as tfstate:
   data = json.load(tfstate)
 
   with open('./credentials.txt', 'a') as credfile:
-    adminpass = data['modules'][0]['resources']['aws_iam_user_login_profile.administrator']['primary']['attributes']['encrypted_password']
-    credfile.write("Administrator Password:   " + adminpass + '\n'
+    encryptpass = data['modules'][0]['resources']['aws_iam_user_login_profile.administrator']['primary']['attributes']['encrypted_password']
+    
+    decodepass = subprocess.Popen(["base64", "--decode"], stdin=encryptpass.stdout, stdout=subprocess.PIPE)
+
+    clearpass = subprocess.Popen(["gpg", "--decrypt"], stdin=decodepass.stdout, stdout=subprocess.PIPE).stdout.read()
+    credfile.write("Administrator Password:   " + clearpasspass + '\n')
     credfile.write("Bob's Access Key:         " + data['modules'][0]['resources']['aws_iam_access_key.bob_key']['primary']['id'] + "\n")
     credfile.write("Bob's Secret Key:         " + data['modules'][0]['resources']['aws_iam_access_key.bob_key']['primary']['attributes']['secret'] + "\n" )
     credfile.write("Joe's Access Key:         " + data['modules'][0]['resources']['aws_iam_access_key.joe_key']['primary']['id'] + "\n")
