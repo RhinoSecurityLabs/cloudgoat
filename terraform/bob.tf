64de0bc7b476f593cb202d7500fecd5b9165d6ed
Spencer Gietzen <spencer.gietzen@rhinosecuritylabs.com>
2018-07-29 15:39:58 -0400

    reformatted start.sh a little. Altered policies for bob, joe, the ec2 instance profile, and the lambda_1 role

--- terraform/bob.tf
+++ terraform/bob.tf
@@ -16,9 +16,25 @@ resource "aws_iam_user_policy" "bob_policy" {
   "Statement": [
     {
       "Action": [
-        "iam:AddUserToGroup",
-        "ec2:*",
-        "iam:UpdateAssumeRolePolicyDocument"
+        "ec2:AllocateAddress",
+        "ec2:AttachVolume",
+        "ec2:CreateDhcpOptions",
+        "ec2:CreateFlowLogs",
+        "ec2:CreateImage",
+        "ec2:CreateRoute",
+        "ec2:DescribeInstances",
+        "ec2:DescribeInstanceAttribute",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeVolumes",
+        "ec2:DescribeVpcs",
+        "ec2:GetConsoleOutput",
+        "ec2:GetConsoleScreenshot",
+        "ec2:GetPasswordData",
+        "ec2:ModifyInstanceAttribute",
+        "ec2:RebootInstances",
+        "ec2:StartInstances",
+        "ec2:StopInstances"
       ],
       "Effect": "Allow",
       "Resource": "*"
--- terraform/joe.tf
+++ terraform/joe.tf
@@ -6,22 +6,7 @@ resource "aws_iam_access_key" "joe_key" {
   user = "${aws_iam_user.joe.name}"
 }
 
-resource "aws_iam_user_policy" "joe_policy" {
-  name = "joe_policy"
-  user = "${aws_iam_user.joe.name}"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:Describe*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+resource "aws_iam_user_policy_attachment" "administrator" {
+  user       = "${aws_iam_user.joe.name}"
+  policy_arn = "arn:aws:iam::aws:policy/job-function/DatabaseAdministrator"
 }
