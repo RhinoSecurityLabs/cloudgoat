246890913752f0517f167c95bacc327bfa1ef376
Spencer Gietzen <spencer@spengietz.com>
2018-08-02 13:20:29 -0400

    fixed random string generation to support Mac

--- start.sh
+++ start.sh
@@ -12,9 +12,11 @@ allowcidr=$1
 mkdir -p ./tmp
 printf $allowcidr > ./tmp/allow_cidr.txt
 
-cloudgoat_public_bucket_name=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
-cloudgoat_private_bucket_name=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
-ec2_web_app_password=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
+# Bucket names, EC2 password, and Glue previously used this command for random generation: $(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
+# But Mac systems get the error: "tr: Illegal byte sequence"
+cloudgoat_public_bucket_name=$(echo $RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM) # 60 chars max (always under 63 char limit)
+cloudgoat_private_bucket_name=$(echo $RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM)
+ec2_web_app_password=$(echo $RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM)
 
 if [[ ! -f ./keys/cloudgoat_key ]]; then
   echo "Creating cloudgoat_key for SSH access."
@@ -48,6 +50,6 @@ terraform apply -auto-approve plan.tfout
 cd .. && ./extract_creds.py
 
 ## Uncomment the follow three lines to enable the Glue development endpoint (make sure to uncomment the specified lines in "kill.sh"
-#glue_dev_endpoint_name=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)
+#glue_dev_endpoint_name=$(echo $RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM)
 #printf $glue_dev_endpoint_name > ./tmp/glue_dev_endpoint_name.txt
 #aws glue create-dev-endpoint --endpoint-name "$glue_dev_endpoint_name" --role-arn "$(< tmp/glue_role_arn.txt)" --number-of-nodes 2 --region us-west-2
