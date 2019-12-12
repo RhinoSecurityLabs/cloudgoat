10fc2ee8dd42974fa330dada65e782f4ef0664a4
Nico Esteves <git@hamstah.com>
2019-12-12 11:39:41 +0100

    Use cgid in resource names for iam_privesc_by_rollback

--- scenarios/iam_privesc_by_rollback/terraform/iam.tf
+++ scenarios/iam_privesc_by_rollback/terraform/iam.tf
@@ -1,23 +1,26 @@
 #IAM Users
 resource "aws_iam_user" "cg-raynor" {
-  name = "raynor"
+  name = "raynor-${var.cgid}"
   tags = {
-    Name = "cg-raynor-${var.cgid}"
-    Stack = "${var.stack-name}"
+    Name     = "cg-raynor-${var.cgid}"
+    Stack    = "${var.stack-name}"
     Scenario = "${var.scenario-name}"
   }
 }
+
 resource "aws_iam_access_key" "cg-raynor" {
   user = "${aws_iam_user.cg-raynor.name}"
 }
+
 #IAM User Policies
 resource "aws_iam_policy" "cg-raynor-policy" {
-  name = "cg-raynor-policy"
+  name        = "cg-raynor-policy-${var.cgid}"
   description = "cg-raynor-policy"
-  policy = "${file("../assets/policies/v1.json")}"
+  policy      = "${file("../assets/policies/v1.json")}"
 }
+
 #IAM Policy Attachments
 resource "aws_iam_user_policy_attachment" "cg-raynor-attachment" {
-  user = "${aws_iam_user.cg-raynor.name}"
+  user       = "${aws_iam_user.cg-raynor.name}"
   policy_arn = "${aws_iam_policy.cg-raynor-policy.arn}"
 }
