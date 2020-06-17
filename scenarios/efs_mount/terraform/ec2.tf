30a05c0a70205a762baa719a8155f5c52e9ccf8f
seb1055 <sebmora1055@gmail.com>
2020-06-17 13:23:34 -0700

    add admin ec2 with efs privs

--- scenarios/efs_mount/terraform/ec2.tf
+++ scenarios/efs_mount/terraform/ec2.tf
@@ -22,6 +22,36 @@ EOF
       Scenario = "${var.scenario-name}"
   }
 }
+
+
+#IAM Admin Role
+
+resource "aws_iam_role" "cg-efs-admin-role" {
+  name = "cg-efs-admin-role-${var.cgid}"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+  tags = {
+      Name = "cg-ec2-efsUser-role-${var.cgid}"
+      Stack = "${var.stack-name}"
+      Scenario = "${var.scenario-name}"
+  }
+}
+
+
+
 #Iam Role Policy
 resource "aws_iam_policy" "cg-ecsTaskExecutionRole-ruse-role-policy" {
   name = "cg-ecsTaskExecutionRole-ruse-role-policy-${var.cgid}"
@@ -47,6 +77,31 @@ resource "aws_iam_policy" "cg-ecsTaskExecutionRole-ruse-role-policy" {
 }
 POLICY
 }
+
+resource "aws_iam_policy" "cg-efs-admin-role-policy" {
+  name = "cg-efs-admin-role-policy-${var.cgid}"
+  description = "cg-efs-admin-role-policy-${var.cgid}"
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+              "elasticfilesystem:ClientMount"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
+}
+
+
+
+
+
 #IAM Role Policy Attachment
 resource "aws_iam_policy_attachment" "cg-ecsTaskExecutionRole-ruse-role-policy-attachment" {
   name = "cg-ecsTaskExecutionRole-ruse-role-policy-attachment-${var.cgid}"
@@ -55,11 +110,27 @@ resource "aws_iam_policy_attachment" "cg-ecsTaskExecutionRole-ruse-role-policy-a
   ]
   policy_arn = "${aws_iam_policy.cg-ecsTaskExecutionRole-ruse-role-policy.arn}"
 }
+
+resource "aws_iam_policy_attachment" "cg-efs-admin-role-policy-attachment" {
+  name = "cg-efs-admin-role-policy-attachment-${var.cgid}"
+  roles = [
+      "${aws_iam_role.cg-efs-admin-role.name}"
+  ]
+  policy_arn = "${aws_iam_policy.cg-efs-admin-role-policy.arn}"
+}
+
+
 #IAM Instance Profile
 resource "aws_iam_instance_profile" "cg-ecsTaskExecutionRole-instance-profile" {
   name = "cg-ecsTaskExecutionRole-instance-profile-${var.cgid}"
   role = "${aws_iam_role.cg-ecsTaskExecutionRole-role.name}"
 }
+
+resource "aws_iam_instance_profile" "cg-efs-admin-instance-profile" {
+  name = "cg-efs-admin-instance-profile-${var.cgid}"
+  role = "${aws_iam_role.cg-efs-admin-role.name}"
+}
+
 #Security Groups
 resource "aws_security_group" "cg-ec2-ssh-security-group" {
   name = "cg-ec2-ssh-${var.cgid}"
@@ -92,7 +163,7 @@ resource "aws_key_pair" "cg-ec2-key-pair" {
   public_key = "${file(var.ssh-public-key-for-ec2)}"
 }
 #EC2 Instance
-resource "aws_instance" "cg-ubuntu-ec2" {
+resource "aws_instance" "cg-ruse-ec2" {
     ami = "ami-0a313d6098716f372"
     instance_type = "t2.micro"
     iam_instance_profile = "${aws_iam_instance_profile.cg-ecsTaskExecutionRole-instance-profile.name}"
@@ -115,6 +186,35 @@ resource "aws_instance" "cg-ubuntu-ec2" {
     }
     tags = {
         Name = "cg-ruse-ec2-${var.cgid}"
+        Stack = "${var.stack-name}"
+        Scenario = "${var.scenario-name}"
+    }
+}
+
+
+resource "aws_instance" "cg-dev-ec2" {
+    ami = "ami-0a313d6098716f372"
+    instance_type = "t2.micro"
+    iam_instance_profile = "${aws_iam_instance_profile.cg-efs-admin-instance-profile.name}"
+    subnet_id = "${aws_subnet.cg-public-subnet-1.id}"
+    associate_public_ip_address = false
+    vpc_security_group_ids = [
+        "${aws_security_group.cg-ec2-ssh-security-group.id}"
+    ]
+    key_name = "${aws_key_pair.cg-ec2-key-pair.key_name}"
+    root_block_device {
+        volume_type = "gp2"
+        volume_size = 8
+        delete_on_termination = true
+    }
+   
+    volume_tags = {
+        Name = "CloudGoat ${var.cgid} EC2 Instance Root Device"
+        Stack = "${var.stack-name}"
+        Scenario = "${var.scenario-name}"
+    }
+    tags = {
+        Name = "cg-efs-admin-ec2-${var.cgid}"
         Stack = "${var.stack-name}"
         Scenario = "${var.scenario-name}"
     }
