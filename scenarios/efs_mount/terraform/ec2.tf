1d317b3d4e85cae0a8883116c831fe8a36ed44e6
seb1055 <sebmora1055@gmail.com>
2020-06-17 11:11:58 -0700

    removing unused ec2 items

--- scenarios/efs_mount/terraform/ec2.tf
+++ scenarios/efs_mount/terraform/ec2.tf
@@ -81,30 +81,7 @@ resource "aws_security_group" "cg-ec2-ssh-security-group" {
     Scenario = "${var.scenario-name}"
   }
 }
-resource "aws_security_group" "cg-ec2-http-security-group" {
-  name = "cg-ec2-http-${var.cgid}"
-  description = "CloudGoat ${var.cgid} Security Group for EC2 Instance over HTTP"
-  vpc_id = "${aws_vpc.cg-vpc.id}"
-  ingress {
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      cidr_blocks = var.cg_whitelist
-  }
-  egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = [
-          "0.0.0.0/0"
-      ]
-  }
-  tags = {
-    Name = "cg-ec2-http-${var.cgid}"
-    Stack = "${var.stack-name}"
-    Scenario = "${var.scenario-name}"
-  }
-}
+
 #AWS Key Pair
 resource "aws_key_pair" "cg-ec2-key-pair" {
   key_name = "cg-ec2-key-pair-${var.cgid}"
@@ -118,8 +95,7 @@ resource "aws_instance" "cg-ubuntu-ec2" {
     subnet_id = "${aws_subnet.cg-public-subnet-1.id}"
     associate_public_ip_address = true
     vpc_security_group_ids = [
-        "${aws_security_group.cg-ec2-ssh-security-group.id}",
-        "${aws_security_group.cg-ec2-http-security-group.id}"
+        "${aws_security_group.cg-ec2-ssh-security-group.id}"
     ]
     key_name = "${aws_key_pair.cg-ec2-key-pair.key_name}"
     root_block_device {
@@ -127,28 +103,7 @@ resource "aws_instance" "cg-ubuntu-ec2" {
         volume_size = 8
         delete_on_termination = true
     }
-    provisioner "file" {
-      source = "../assets/ssrf_app/app.zip"
-      destination = "/home/ubuntu/app.zip"
-      connection {
-        type = "ssh"
-        user = "ubuntu"
-        private_key = "${file(var.ssh-private-key-for-ec2)}"
-        host = self.public_ip
-      }
-    }
-    user_data = <<-EOF
-        #!/bin/bash
-        apt-get update
-        curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-        DEBIAN_FRONTEND=noninteractive apt-get -y install nodejs unzip
-        npm install http express needle command-line-args
-        cd /home/ubuntu
-        unzip app.zip -d ./app
-        cd app
-        sudo node ssrf-demo-app.js &
-        echo -e "\n* * * * * root node /home/ubuntu/app/ssrf-demo-app.js &\n* * * * * root sleep 10; node /home/ubuntu/app/ssrf-demo-app.js &\n* * * * * root sleep 20; node /home/ubuntu/app/ssrf-demo-app.js &\n* * * * * root sleep 30; node /home/ubuntu/app/ssrf-demo-app.js &\n* * * * * root sleep 40; node /home/ubuntu/app/ssrf-demo-app.js &\n* * * * * root sleep 50; node /home/ubuntu/app/ssrf-demo-app.js &\n" >> /etc/crontab
-        EOF
+   
     volume_tags = {
         Name = "CloudGoat ${var.cgid} EC2 Instance Root Device"
         Stack = "${var.stack-name}"
