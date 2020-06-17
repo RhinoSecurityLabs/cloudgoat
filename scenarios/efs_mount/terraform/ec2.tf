0e9e466d4d957c55caf3b2b020c12ca9280a7621
seb1055 <sebmora1055@gmail.com>
2020-06-17 13:09:19 -0700

    Added basic permissions for first ec2

--- scenarios/efs_mount/terraform/ec2.tf
+++ scenarios/efs_mount/terraform/ec2.tf
@@ -1,5 +1,5 @@
 #IAM Role
-resource "aws_iam_role" "cg-ec2-role" {
+resource "aws_iam_role" "cg-ecsTaskExecutionRole-role" {
   name = "cg-ec2-role-${var.cgid}"
   assume_role_policy = <<EOF
 {
@@ -8,7 +8,7 @@ resource "aws_iam_role" "cg-ec2-role" {
     {
       "Action": "sts:AssumeRole",
       "Principal": {
-        "Service": "ec2.amazonaws.com"
+        "Service": "ecs-tasks.amazonaws.com"
       },
       "Effect": "Allow",
       "Sid": ""
@@ -23,9 +23,9 @@ EOF
   }
 }
 #Iam Role Policy
-resource "aws_iam_policy" "cg-ec2-role-policy" {
-  name = "cg-ec2-role-policy-${var.cgid}"
-  description = "cg-ec2-role-policy-${var.cgid}"
+resource "aws_iam_policy" "cg-ecsTaskExecutionRole-ruse-role-policy" {
+  name = "cg-ecsTaskExecutionRole-ruse-role-policy-${var.cgid}"
+  description = "cg-ecsTaskExecutionRole-ruse-role-policy-${var.cgid}"
   policy = <<POLICY
 {
     "Version": "2012-10-17",
@@ -34,8 +34,12 @@ resource "aws_iam_policy" "cg-ec2-role-policy" {
             "Sid": "VisualEditor0",
             "Effect": "Allow",
             "Action": [
-                "s3:*",
-                "cloudwatch:*"
+              "ecr:GetAuthorizationToken",
+              "ecr:BatchCheckLayerAvailability",
+              "ecr:GetDownloadUrlForLayer",
+              "ecr:BatchGetImage",
+              "logs:CreateLogStream",
+              "logs:PutLogEvents"
             ],
             "Resource": "*"
         }
@@ -44,17 +48,17 @@ resource "aws_iam_policy" "cg-ec2-role-policy" {
 POLICY
 }
 #IAM Role Policy Attachment
-resource "aws_iam_policy_attachment" "cg-ec2-role-policy-attachment" {
-  name = "cg-ec2-role-policy-attachment-${var.cgid}"
+resource "aws_iam_policy_attachment" "cg-ecsTaskExecutionRole-ruse-role-policy-attachment" {
+  name = "cg-ecsTaskExecutionRole-ruse-role-policy-attachment-${var.cgid}"
   roles = [
-      "${aws_iam_role.cg-ec2-role.name}"
+      "${aws_iam_role.cg-ecsTaskExecutionRole-role.name}"
   ]
-  policy_arn = "${aws_iam_policy.cg-ec2-role-policy.arn}"
+  policy_arn = "${aws_iam_policy.cg-ecsTaskExecutionRole-ruse-role-policy.arn}"
 }
 #IAM Instance Profile
-resource "aws_iam_instance_profile" "cg-ec2-instance-profile" {
-  name = "cg-ec2-instance-profile-${var.cgid}"
-  role = "${aws_iam_role.cg-ec2-role.name}"
+resource "aws_iam_instance_profile" "cg-ecsTaskExecutionRole-instance-profile" {
+  name = "cg-ecsTaskExecutionRole-instance-profile-${var.cgid}"
+  role = "${aws_iam_role.cg-ecsTaskExecutionRole-role.name}"
 }
 #Security Groups
 resource "aws_security_group" "cg-ec2-ssh-security-group" {
@@ -91,7 +95,7 @@ resource "aws_key_pair" "cg-ec2-key-pair" {
 resource "aws_instance" "cg-ubuntu-ec2" {
     ami = "ami-0a313d6098716f372"
     instance_type = "t2.micro"
-    iam_instance_profile = "${aws_iam_instance_profile.cg-ec2-instance-profile.name}"
+    iam_instance_profile = "${aws_iam_instance_profile.cg-ecsTaskExecutionRole-instance-profile.name}"
     subnet_id = "${aws_subnet.cg-public-subnet-1.id}"
     associate_public_ip_address = true
     vpc_security_group_ids = [
@@ -110,7 +114,7 @@ resource "aws_instance" "cg-ubuntu-ec2" {
         Scenario = "${var.scenario-name}"
     }
     tags = {
-        Name = "cg-ubuntu-ec2-${var.cgid}"
+        Name = "cg-ruse-ec2-${var.cgid}"
         Stack = "${var.stack-name}"
         Scenario = "${var.scenario-name}"
     }
