c97fc1d8e3ddda467430d90ca98db01a8aaace28
seb <seb@ruse.tech>
2021-06-04 10:55:11 -0700

    adding whitelist for resources

--- scenarios/ecs_takeover/terraform/vpc.tf
+++ scenarios/ecs_takeover/terraform/vpc.tf
@@ -106,21 +106,21 @@ resource "aws_security_group" "ecs_sg" {
         from_port       = 22
         to_port         = 22
         protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
+        cidr_blocks     = [var.cg_whitelist]
     }
 
     ingress {
         from_port       = 443
         to_port         = 443
         protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
+        cidr_blocks     = [var.cg_whitelist]
     }
 
     egress {
         from_port       = 0
         to_port         = 65535
         protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
+        cidr_blocks     = [var.cg_whitelist]
     }
 }
 
@@ -132,20 +132,20 @@ resource "aws_security_group" "alb_sg" {
         from_port       = 80
         to_port         = 80
         protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
+        cidr_blocks     = [var.cg_whitelist]
     }
 
     ingress {
         from_port       = 443
         to_port         = 443
         protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
+        cidr_blocks     = [var.cg_whitelist]
     }
 
     egress {
         from_port       = 0
         to_port         = 65535
         protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
+        cidr_blocks     = [var.cg_whitelist]
     }
 }
