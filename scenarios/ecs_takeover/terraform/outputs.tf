a4878240bbdaae23723d009138e3ff1f9399da67
seb <seb@ruse.tech>
2021-06-14 14:35:41 -0700

    Added start up note

--- scenarios/ecs_takeover/terraform/outputs.tf
+++ scenarios/ecs_takeover/terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "vuln-site" {
   value = aws_alb.website-alb.dns_name
 }
+
+output "Start-Note"{
+  value = "If a 503 error is returned by the ALB give a few mins for the website container to become active."
+}
