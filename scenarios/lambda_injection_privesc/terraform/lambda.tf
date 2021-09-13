048d47e3f1d7453209c891d32c2153cca74da24b
cmd-crtl-freq <davidfentz@protonmail.com>
2021-09-13 12:36:52 -0700

    updated the iam roles for each lambda function

--- scenarios/lambda_injection_privesc/terraform/lambda.tf
+++ scenarios/lambda_injection_privesc/terraform/lambda.tf
@@ -1,6 +1,25 @@
 #role-creator-lambda
 resource "aws_iam_role" "policy_applier_lambda" {
   name = "cg-${var.cgid}-policy_applier_lambda"
+  inline_policy {
+    name = "my_inline_policy"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = "iam:AttachUserPolicy"
+          Effect   = "Allow"
+          Resource = "${aws_iam_user.bilbo.arn}"
+        },
+        {
+          Action   = "s3:GetObject"
+          Effect   = "Allow"
+          Resource = "*"
+        }
+      ]
+    })
+  }
+
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -32,7 +51,7 @@ data "archive_file" "policy_applier_lambda_zip" {
 resource "aws_lambda_function" "policy_applier_lambda" {
   filename      = "${data.archive_file.policy_applier_lambda_zip.output_path}"
   function_name = "cg-${var.cgid}-policy_applier_lambda"
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = aws_iam_role.policy_applier_lambda.arn
   handler       = "main.handler"
   description   =  "This function will apply a managed policy to the user of your choice, so long as the database says that it's okay..."
   source_code_hash = filebase64sha256("${data.archive_file.policy_applier_lambda_zip.output_path}")
@@ -46,7 +65,7 @@ resource "aws_lambda_function" "policy_applier_lambda" {
 
 
 
-#invokation-target-lambda
+#invocation-target-lambda
 resource "aws_iam_role" "target_lambda" {
   name = "cg-${var.cgid}-target_lambda"
   assume_role_policy = <<EOF
@@ -80,7 +99,7 @@ data "archive_file" "target_lambda_zip" {
 resource "aws_lambda_function" "target_lambda" {
   filename      = "${data.archive_file.target_lambda_zip.output_path}"
   function_name = "cg-${var.cgid}-target_lambda"
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = aws_iam_role.target_lambda.arn
   handler       = "main.handler"
   description   = "Invoke this function correctly and you win this scenario."
   source_code_hash = filebase64sha256("${data.archive_file.target_lambda_zip.output_path}")
