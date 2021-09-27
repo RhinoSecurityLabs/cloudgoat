929053f1021aff32658fd02779eefa063ee6d04b
David FEntz <davidfentz@protonmail.com>
2021-09-27 12:56:03 -0700

    the script for removing attached user policies now installs jq if it is not present.

--- scenarios/lambda_injection_privesc/terraform/resource_cleaning.sh
+++ scenarios/lambda_injection_privesc/terraform/resource_cleaning.sh
@@ -1,3 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found"
+    echo "installing jq"
+    sudo apt install jq
+    exit
+fi
+
+
 
 aws iam list-attached-user-policies --user-name $1 | jq '.AttachedPolicies | .[] | .PolicyArn' | xargs -i aws iam detach-user-policy --user-name $1 --policy-arn {}
