5e0eb541f1d02671471fa5c7a4f05deb2a466f63
David FEntz <davidfentz@protonmail.com>
2021-10-05 12:56:48 -0700

    updated terraform script with a suggestion from ryan.

--- scenarios/lambda_injection_privesc/terraform/resource_cleaning.sh
+++ scenarios/lambda_injection_privesc/terraform/resource_cleaning.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-
-if ! command -v jq &> /dev/null
-then
-    echo "jq could not be found"
-    echo "installing jq"
-    sudo apt install jq
-    exit
-fi
-
-
-
-aws iam list-attached-user-policies --user-name $1 | jq '.AttachedPolicies | .[] | .PolicyArn' | xargs -i aws iam detach-user-policy --user-name $1 --policy-arn {}
+aws iam list-attached-user-policies --user-name $1 --query  'AttachedPolicies[].PolicyArn' --out text | xargs -n 1 aws iam detach-user-policy --user-name $1 --policy-arn
