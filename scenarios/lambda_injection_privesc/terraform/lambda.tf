399fd501a8628aedf6752bc08f3b2512ccfdfaa4
David FEntz <davidfentz@protonmail.com>
2021-10-10 14:48:34 -0700

    added a stub of the second lambda function. fixed a bug where the scenario would not destroy if there were no policies attached to the bilbo user.

--- scenarios/lambda_injection_privesc/terraform/lambda.tf
+++ scenarios/lambda_injection_privesc/terraform/lambda.tf
@@ -72,9 +72,9 @@ resource "aws_lambda_function" "policy_applier_lambda" {
 
 
 
-#invocation-target-lambda
-resource "aws_iam_role" "target_lambda" {
-  name = "cg-${var.cgid}-target_lambda"
+#invocation-target-lambda1
+resource "aws_iam_role" "target_lambda1" {
+  name = "cg-${var.cgid}-target_lambda1"
 
   assume_role_policy = <<EOF
 {
@@ -93,18 +93,56 @@ resource "aws_iam_role" "target_lambda" {
 EOF
 }
 
-data "archive_file" "target_lambda_zip" {
+data "archive_file" "target_lambda1_zip" {
     type        = "zip"
-    source_dir  = "lambda_source_code/target_lambda_src"
-    output_path = "lambda_source_code/archives/target_lambda_src.zip"
+    source_dir  = "lambda_source_code/target_lambda1_src"
+    output_path = "lambda_source_code/archives/target_lambda1_src.zip"
 }
 
-resource "aws_lambda_function" "target_lambda" {
-  filename      = data.archive_file.target_lambda_zip.output_path
-  function_name = "cg-${var.cgid}-target_lambda"
-  role          = aws_iam_role.target_lambda.arn
+resource "aws_lambda_function" "target_lambda1" {
+  filename      = data.archive_file.target_lambda1_zip.output_path
+  function_name = "cg-${var.cgid}-target_lambda1"
+  role          = aws_iam_role.target_lambda1.arn
   handler       = "main.handler"
   description   = "Invoke this function correctly and you win this scenario."
-  source_code_hash = filebase64sha256(data.archive_file.target_lambda_zip.output_path)
+  source_code_hash = filebase64sha256(data.archive_file.target_lambda1_zip.output_path)
+  runtime = "python3.9"
+}
+
+
+#invocation-target-lambda2
+resource "aws_iam_role" "target_lambda2" {
+  name = "cg-${var.cgid}-target_lambda2"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+data "archive_file" "target_lambda2_zip" {
+    type        = "zip"
+    source_dir  = "lambda_source_code/target_lambda2_src"
+    output_path = "lambda_source_code/archives/target_lambda2_src.zip"
+}
+
+resource "aws_lambda_function" "target_lambda2" {
+  filename      = data.archive_file.target_lambda2_zip.output_path
+  function_name = "cg-${var.cgid}-target_lambda2"
+  role          = aws_iam_role.target_lambda2.arn
+  handler       = "main.handler"
+  description   = "Invoke this function correctly and you win this scenario."
+  source_code_hash = filebase64sha256(data.archive_file.target_lambda2_zip.output_path)
   runtime = "python3.9"
 }
