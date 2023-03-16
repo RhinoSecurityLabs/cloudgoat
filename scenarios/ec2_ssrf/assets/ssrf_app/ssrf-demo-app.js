a8e491a352e6a58f598b5bb14fa6036666e1e0d5
John De Armas <john.dearmas@rhinosecuritylabs.com>
2023-03-15 22:57:25 -0600

    Fix: #103 ec2_ssrf error on default page

--- scenarios/ec2_ssrf/assets/ssrf_app/ssrf-demo-app.js
+++ scenarios/ec2_ssrf/assets/ssrf_app/ssrf-demo-app.js
@@ -29,23 +29,32 @@ app.get('/', function(request, response){
 
     console.log('New request: '+request.url);
 
-    needle.get(url, { timeout: 3000 }, function(error, response1) {
-      if (!error && response1.statusCode == 200) {
-        response.writeHead(200, {'Content-Type': 'text/'+mime});
-        response.write('<h1>Welcome to sethsec\'s SSRF demo.</h1>\n\n');
-        response.write('<h2>I am an application. I want to be useful, so I requested: <font color="red">'+url+'</font> for you\n</h2><br><br>\n\n\n');
-        console.log(response1.body);
-        response.write(response1.body);
-        response.end();
-      } else {
-        response.writeHead(404, {'Content-Type': 'text/'+mime});
-        response.write('<h1>Welcome to sethsec\'s SSRF demo.</h1>\n\n');
-        response.write('<h2>I wanted to be useful, but I could not find: <font color="red">'+url+'</font> for you\n</h2><br><br>\n\n\n');
-        response.end();
-        console.log('error')
+    // If the URL is not set, then we will just return the default page.
+    if (url == undefined) {
+      response.writeHead(200, {'Content-Type': 'text/'+mime});
+      response.write('<h1>Welcome to sethsec\'s SSRF demo.</h1>\n\n');
+      response.write('<h2>I am an application. I want to be useful, so give me a URL to requested for you\n</h2><br><br>\n\n\n');
+      response.end();
+    } else { // If the URL is set, then we will try to request it.
+      needle.get(url, { timeout: 3000 }, function(error, response1) {
+        // If the request is successful, then we will return the response to the user.
+        if (!error && response1.statusCode == 200) {
+          response.writeHead(200, {'Content-Type': 'text/'+mime});
+          response.write('<h1>Welcome to sethsec\'s SSRF demo.</h1>\n\n');
+          response.write('<h2>I am an application. I want to be useful, so I requested: <font color="red">'+url+'</font> for you\n</h2><br><br>\n\n\n');
+          console.log(response1.body);
+          response.write(response1.body);
+          response.end();
+        } else { // If the request is not successful, then we will return an error to the user.
+          response.writeHead(404, {'Content-Type': 'text/'+mime});
+          response.write('<h1>Welcome to sethsec\'s SSRF demo.</h1>\n\n');
+          response.write('<h2>I wanted to be useful, but I could not find: <font color="red">'+url+'</font> for you\n</h2><br><br>\n\n\n');
+          response.end();
+          console.log('error')
 
       }
     });
+  }
 })
 if (options.port) {
 	var port = options.port
