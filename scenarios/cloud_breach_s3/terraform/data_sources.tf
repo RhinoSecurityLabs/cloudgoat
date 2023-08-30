feb20e73b5b7daf2889a305885705a3e026fe70a
Andrew Aiken <andrew.aiken@machinemetrics.com>
2023-08-30 11:04:32 -0400

    update cloud breach S3 Terraform

--- scenarios/cloud_breach_s3/terraform/data_sources.tf
+++ scenarios/cloud_breach_s3/terraform/data_sources.tf
@@ -1,8 +1,2 @@
 #AWS Account Id
-data "aws_caller_identity" "aws-account-id" {
-
-}
-#S3 Full Access Policy
-data "aws_iam_policy" "s3-full-access" {
-  arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
+data "aws_caller_identity" "aws-account-id" {}
--- scenarios/cloud_breach_s3/terraform/ec2.tf
+++ scenarios/cloud_breach_s3/terraform/ec2.tf
@@ -1,33 +1,31 @@
 #IAM Role
 resource "aws_iam_role" "cg-banking-WAF-Role" {
-  name               = "cg-banking-WAF-Role-${var.cgid}"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+  name = "cg-banking-WAF-Role-${var.cgid}"
+  assume_role_policy = jsonencode(
     {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Principal = {
+            Service = "ec2.amazonaws.com"
+          }
+          Effect = "Allow"
+          Sid    = ""
+        }
+      ]
     }
+  )
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess"
   ]
-}
-EOF
-  tags = {
-    Name     = "cg-banking-WAF-Role-${var.cgid}"
-    Stack    = "${var.stack-name}"
-    Scenario = "${var.scenario-name}"
-  }
+
+  tags = merge(local.default_tags, {
+    Name = "cg-banking-WAF-Role-${var.cgid}"
+  })
 }
 
-#IAM Role Policy Attachment
-resource "aws_iam_role_policy_attachment" "cg-banking-WAF-Role-policy-attachment-s3" {
-  role       = aws_iam_role.cg-banking-WAF-Role.name
-  policy_arn = data.aws_iam_policy.s3-full-access.arn
-}
 
 #IAM Instance Profile
 resource "aws_iam_instance_profile" "cg-ec2-instance-profile" {
@@ -40,12 +38,14 @@ resource "aws_security_group" "cg-ec2-ssh-security-group" {
   name        = "cg-ec2-ssh-${var.cgid}"
   description = "CloudGoat ${var.cgid} Security Group for EC2 Instance over SSH"
   vpc_id      = aws_vpc.cg-vpc.id
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = var.cg_whitelist
   }
+
   egress {
     from_port = 0
     to_port   = 0
@@ -54,22 +54,24 @@ resource "aws_security_group" "cg-ec2-ssh-security-group" {
       "0.0.0.0/0"
     ]
   }
-  tags = {
-    Name     = "cg-ec2-ssh-${var.cgid}"
-    Stack    = "${var.stack-name}"
-    Scenario = "${var.scenario-name}"
-  }
+
+  tags = merge(local.default_tags, {
+    Name = "cg-ec2-ssh-${var.cgid}"
+  })
 }
+
 resource "aws_security_group" "cg-ec2-http-security-group" {
   name        = "cg-ec2-http-${var.cgid}"
   description = "CloudGoat ${var.cgid} Security Group for EC2 Instance over HTTP"
   vpc_id      = aws_vpc.cg-vpc.id
+
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = var.cg_whitelist
   }
+
   egress {
     from_port = 0
     to_port   = 0
@@ -78,17 +80,18 @@ resource "aws_security_group" "cg-ec2-http-security-group" {
       "0.0.0.0/0"
     ]
   }
-  tags = {
-    Name     = "cg-ec2-http-${var.cgid}"
-    Stack    = "${var.stack-name}"
-    Scenario = "${var.scenario-name}"
-  }
+
+  tags = merge(local.default_tags, {
+    Name = "cg-ec2-http-${var.cgid}"
+  })
 }
+
 #AWS Key Pair
 resource "aws_key_pair" "cg-ec2-key-pair" {
   key_name   = "cg-ec2-key-pair-${var.cgid}"
   public_key = file(var.ssh-public-key-for-ec2)
 }
+
 #EC2 Instance
 resource "aws_instance" "ec2-vulnerable-proxy-server" {
   ami                         = "ami-0a313d6098716f372"
@@ -96,16 +99,19 @@ resource "aws_instance" "ec2-vulnerable-proxy-server" {
   iam_instance_profile        = aws_iam_instance_profile.cg-ec2-instance-profile.name
   subnet_id                   = aws_subnet.cg-public-subnet-1.id
   associate_public_ip_address = true
+
   vpc_security_group_ids = [
-    "${aws_security_group.cg-ec2-ssh-security-group.id}",
-    "${aws_security_group.cg-ec2-http-security-group.id}"
+    aws_security_group.cg-ec2-ssh-security-group.id,
+    aws_security_group.cg-ec2-http-security-group.id
   ]
+
   key_name = aws_key_pair.cg-ec2-key-pair.key_name
   root_block_device {
     volume_type           = "gp2"
     volume_size           = 8
     delete_on_termination = true
   }
+
   provisioner "file" {
     source      = "../assets/proxy.com"
     destination = "/home/ubuntu/proxy.com"
@@ -116,6 +122,7 @@ resource "aws_instance" "ec2-vulnerable-proxy-server" {
       host        = self.public_ip
     }
   }
+
   user_data = <<-EOF
         #!/bin/bash
         apt-get update
@@ -125,14 +132,12 @@ resource "aws_instance" "ec2-vulnerable-proxy-server" {
         rm /etc/nginx/sites-enabled/default
         systemctl restart nginx
         EOF
-  volume_tags = {
-    Name     = "CloudGoat ${var.cgid} EC2 Instance Root Device"
-    Stack    = "${var.stack-name}"
-    Scenario = "${var.scenario-name}"
-  }
-  tags = {
-    Name     = "ec2-vulnerable-proxy-server-${var.cgid}"
-    Stack    = "${var.stack-name}"
-    Scenario = "${var.scenario-name}"
-  }
+
+  volume_tags = merge(local.default_tags, {
+    Name = "CloudGoat ${var.cgid} EC2 Instance Root Device"
+  })
+
+  tags = merge(local.default_tags, {
+    Name = "ec2-vulnerable-proxy-server-${var.cgid}"
+  })
 }
