75a6b3d8d6f8a9925fddf509ac82a4a48032f688
2W <hsp003636@gmail.com>
2023-11-14 03:04:19 +0900

    add ETP_JOB.py in s3 & fix connections

--- scenarios/glue_privesc/terraform/glue.tf
+++ scenarios/glue_privesc/terraform/glue.tf
@@ -14,7 +14,6 @@ resource "aws_glue_connection" "cg-glue-connection" {
     security_group_id_list = [aws_security_group.cg-rds-glue-security-group.id]
     subnet_id              = aws_subnet.cg-public-subnet-1.id
   }
-
 }
 
 resource "aws_glue_job" "cg-glue-job" {
@@ -22,15 +21,20 @@ resource "aws_glue_job" "cg-glue-job" {
   role_arn = aws_iam_role.glue_ETL_role.arn
 
   command {
-    script_location = "../assets/ETL_JOB.py"
+    script_location = "s3://${aws_s3_bucket.cg-data-s3-bucket.bucket}/${aws_s3_object.glue_script_file.key}"
     python_version  = "3"
     name            = "ETL_JOB"
   }
 
-  connections = ["test-connections"]
+  connections = [aws_glue_connection.cg-glue-connection.name] # 위에서 정의한 Glue 연결 사용
 
-  max_retries = 1
+  max_retries = 3
 
   timeout = 60
 }
 
+resource "aws_s3_object" "glue_script_file" {
+  bucket = aws_s3_bucket.cg-data-s3-bucket.id
+  key    = "glue_ETL_JOB"
+  source = "../assets/ETL_JOB.py"
+}
