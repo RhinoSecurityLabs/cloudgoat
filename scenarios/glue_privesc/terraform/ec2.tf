9554886d3bc84fcde4257b25b19c071cbe20c5a7
hosim33 <ghtla0330@naver.com>
2023-11-14 14:13:51 +0900

    1114 init error fix

--- scenarios/glue_privesc/terraform/ec2.tf
+++ scenarios/glue_privesc/terraform/ec2.tf
@@ -1,21 +1,21 @@
 #임시키- 나중에 삭제
 resource "aws_key_pair" "bob-ec2-key-pair" {
   key_name   = "cg-ec2-key-pair-${var.cgid}"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = "${file(var.ssh-public-key-for-ec2)}"
 }
 
 resource "aws_instance" "cg-ubuntu-ec2" {
   ami                         = "ami-05c13eab67c5d8861"
   instance_type               = "t2.micro"
-  iam_instance_profile        = aws_iam_instance_profile.cg-ec2-instance-profile.name
-  subnet_id                   = aws_subnet.cg-public-subnet-1.id
+  iam_instance_profile        = "${aws_iam_instance_profile.cg-ec2-instance-profile.name}"
+  subnet_id                   = "${aws_subnet.cg-public-subnet-1.id}"
   associate_public_ip_address = true
 
   vpc_security_group_ids = [
     "${aws_security_group.cg-rds-glue-security-group.id}"
     # aws_security_group.cg-ec2-rds-security-group.id
   ]
-  key_name = aws_key_pair.bob-ec2-key-pair.key_name
+  key_name = "${aws_key_pair.bob-ec2-key-pair.key_name}"
   root_block_device {
     volume_type           = "gp3"
     volume_size           = 8
@@ -29,7 +29,7 @@ resource "aws_instance" "cg-ubuntu-ec2" {
     connection {
       type        = "ssh"
       user        = "ec2-user"
-      private_key = file(var.ssh-private-key-for-ec2)
+      private_key = "${file(var.ssh-private-key-for-ec2)}"
       host        = self.public_ip
     }
   }
--- scenarios/glue_privesc/terraform/lambda.tf
+++ scenarios/glue_privesc/terraform/lambda.tf
@@ -13,14 +13,20 @@ resource "aws_lambda_function" "s3_to_gluecatalog" {
   filename         = "../assets/s3_to_gluecatalog.zip"
   source_code_hash = filebase64sha256("../assets/s3_to_gluecatalog.py")
 
-  timeout = 180
+  timeout = 300
+
+  triggers {
+    type = "s3"
+    source_arns = [aws_s3_bucket.cg-data-from-web.arn]
+    events = ["s3:ObjectCreated:Put"]
+  }
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.cg-data-from-web.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.s3_to_gluecatalog.arn
+    lambda_function_arn = "${aws_lambda_function.s3_to_gluecatalog.arn}"
     events              = ["s3:ObjectCreated:Put"]
   }
 
@@ -30,7 +36,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 resource "aws_lambda_permission" "allow_bucket" {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.s3_to_gluecatalog.function_name
+  function_name = "${aws_lambda_function.s3_to_gluecatalog.function_name}"
   principal     = "s3.amazonaws.com"
   source_arn    = "${aws_s3_bucket.cg-data-from-web.arn}/*"
 }
