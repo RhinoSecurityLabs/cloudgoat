dddca5789bc68cf3eac91be3d90bc970a764ae42
hosim33 <ghtla0330@naver.com>
2023-11-16 00:58:46 +0900

    user-data sleep add

--- scenarios/glue_privesc/terraform/ec2.tf
+++ scenarios/glue_privesc/terraform/ec2.tf
@@ -57,6 +57,7 @@ resource "aws_instance" "cg-linux-ec2" {
         sudo yum install -y postgresql15.x86_64
 
         psql postgresql://${aws_db_instance.cg-rds.username}:${aws_db_instance.cg-rds.password}@${aws_db_instance.cg-rds.endpoint}/${aws_db_instance.cg-rds.db_name} -f /home/ec2-user/insert_data.sql
+        sleep 60
 
         pip install Flask 
         pip install boto3
