d381c10264afa497e7d5ec7050df9aedf668bad8
2W <hsp003636@gmail.com>
2023-11-20 01:17:19 +0900

    combine template_file block and local_file

--- scenarios/glue_privesc/terraform/rds.tf
+++ scenarios/glue_privesc/terraform/rds.tf
@@ -32,46 +32,14 @@ data "local_file" "csv_file" {
   filename = "../assets/order_data2.csv"
 }
 
-#data "template_file" "sql_template" {
-#  template = <<-EOT
-#
-#    CREATE TABLE original_data (
-#        order_date VARCHAR(255),
-#        item_id VARCHAR(255),
-#        price numeric(10,2),
-#        country_code VARCHAR(50)
-#    );
-#
-#    CREATE TABLE cc_data (
-#        country_code VARCHAR(50),
-#        purchase_cnt int,
-#        avg_price numeric(10,2)
-#    );
-#
-#    %{for row in csvdecode(data.local_file.csv_file.content)}
-#            INSERT INTO original_data (order_date, item_id, price, country_code) VALUES ('${row.order_date}', '${row.item_id}', ${format("%.2f", row.price)}, '${row.country_code}');
-#    %{endfor}
-#
-#    INSERT INTO original_data (order_date, item_id, price, country_code) VALUES ('${var.rds_user_key}', '${var.rds_user_secret}', DEFAULT, DEFAULT);
-#  EOT
-#}
-
-
-data "template_file" "sql_template" {
-  template = templatefile("${path.module}/../assets/sql_template.tpl", {
+resource "local_file" "sql_file" {
+  content  = templatefile("${path.module}/../assets/sql_template.tpl", {
     csv_content           = data.local_file.csv_file.content,
     aws_access_key_id     = aws_iam_access_key.cg-glue-admin_access_key.id,
     aws_secret_access_key = aws_iam_access_key.cg-glue-admin_access_key.secret
   })
-}
-
-
-resource "local_file" "sql_file" {
-  content  = data.template_file.sql_template.rendered
   filename = "../assets/insert_data.sql"
 }
-
-
 
 resource "aws_db_subnet_group" "cg-rds-subnet-group" {
   name = "cg-rds-subnet-group-${var.cgid}"
