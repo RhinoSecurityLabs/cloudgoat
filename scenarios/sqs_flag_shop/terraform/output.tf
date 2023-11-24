d05d685503887a8e77d85804452ed40b809423f4
hosim33 <ghtla0330@naver.com>
2023-11-24 15:02:55 +0900

    sqs user output add

--- scenarios/sqs_flag_shop/terraform/output.tf
+++ scenarios/sqs_flag_shop/terraform/output.tf
@@ -1,3 +1,12 @@
+output "cloudgoat_output_sqsuser_access_key_id" {
+  value = aws_iam_access_key.cg-sqs-user_access_key.id
+}
+
+output "cloudgoat_output_sqsuser_secret_key" {
+  value     = aws_iam_access_key.cg-sqs-user_access_key.secret
+  sensitive = true
+}
+
 output "cg_web_site_ip" {
   value = "${aws_instance.cg_flag_shop_server.public_ip}:5000"
 }
