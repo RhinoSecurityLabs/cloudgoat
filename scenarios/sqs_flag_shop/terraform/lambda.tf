eb7671f4fbcba3c52b3358139a5ab01a666922e3
hosim33 <ghtla0330@naver.com>
2023-12-02 14:42:49 +0900

    lambda.tf edit

--- scenarios/sqs_flag_shop/terraform/lambda.tf
+++ scenarios/sqs_flag_shop/terraform/lambda.tf
@@ -1,11 +1,17 @@
+data "archive_file" "cg-lambda-function" {
+  type        = "zip"
+  source_file = "../assets/lambda_function.py"
+  output_path = "../assets/lambda_function.zip"
+}
+
 resource "aws_lambda_function" "charging_cash_lambda" {
   function_name = "lambda_function"
-  handler       = "charging_cash_lambda/lambda_function.lambda_handler"
+  handler       = "lambda_function.lambda_handler"
   role          = aws_iam_role.cg-sqs_lambda_role.arn
   runtime       = "python3.11"
 
-  filename         = "${path.module}/../assets/lambda_function.py"
-  source_code_hash = filebase64sha256("${path.module}/../assets/lambda_funtion.py")
+  filename         = data.archive_file.cg-lambda-function.output_path
+  source_code_hash = filebase64sha256(data.archive_file.cg-lambda-function.output_path)
 
   depends_on = [aws_vpc.cg-vpc]
 
