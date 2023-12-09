6df152b7ef0e406f6a04871944aae85b49464dd1
2W <hsp003636@gmail.com>
2023-12-09 11:50:20 +0900

    fix error | S3_bucket_policy Access Denied

--- scenarios/glue_privesc/terraform/s3.tf
+++ scenarios/glue_privesc/terraform/s3.tf
@@ -31,7 +31,6 @@ resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
 }
 
 # web to s3
-# test-glue-scenario2
 resource "aws_s3_bucket" "cg-data-from-web" {
   bucket        = "cg-data-from-web-${local.bucket_suffix}"
   force_destroy = true
@@ -65,22 +64,19 @@ resource "aws_s3_bucket_public_access_block" "access_block" {
 }
 
 
-resource "aws_s3_bucket_policy" "putobject" {
+resource "aws_s3_bucket_policy" "put_object" {
   bucket = aws_s3_bucket.cg-data-from-web.id
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Statement1",
-      "Action": [
-        "s3:PutObject"
-      ],
-      "Effect": "Allow",
-      "Resource": "${aws_s3_bucket.cg-data-from-web.arn}/*",
-      "Principal": "*"
-    }
-  ]
-}
-POLICY
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "Statement1"
+        Action    = ["s3:PutObject"]
+        Effect    = "Allow"
+        Resource  = "${aws_s3_bucket.cg-data-from-web.arn}/*"
+        Principal = "*"
+      }
+    ]
+  })
+  depends_on = [aws_s3_bucket_public_access_block.access_block]
 }
