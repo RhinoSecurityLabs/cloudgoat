48a1e0af5c5c5ec1a26cb9cbad41bb82134a6117
2W <hsp003636@gmail.com>
2023-12-09 12:00:07 +0900

    modify ouput

--- scenarios/glue_privesc/terraform/outputs.tf
+++ scenarios/glue_privesc/terraform/outputs.tf
@@ -1,7 +1,3 @@
 output "cg_web_site_ip" {
-  value = aws_instance.cg-linux-ec2.public_ip
-}
-
-output "cg_web_site_port" {
-  value = 5000
+  value = "${aws_instance.cg-linux-ec2.public_ip}:5000"
 }
