93c64969396794960b9c8b89229838f324f63318
3iuy <siuy2685@gmail.com>
2023-12-15 11:16:54 +0900

    Once the scripts would be fail, make tf fail.

--- scenarios/ecs_privesc_evade_protection/terraform/ecr.tf
+++ scenarios/ecs_privesc_evade_protection/terraform/ecr.tf
@@ -6,14 +6,16 @@ resource "null_resource" "docker_image" {
 
   # Push Docker image when the scenario be created
   provisioner "local-exec" {
-    when    = create
-    command = "python ./push-dockerfile.py --dockerfile_path ../assets/ssrf-web/ --repository ${aws_ecr_repository.repository.name} --region ${var.region} --profile ${var.profile} --image_tag latest"
+    when       = create
+    on_failure = fail
+    command    = "python ./push-dockerfile.py --dockerfile_path ../assets/ssrf-web/ --repository ${aws_ecr_repository.repository.name} --region ${var.region} --profile ${var.profile} --image_tag latest"
   }
 
   # Pop Docker images when the scenario be destroyed
   provisioner "local-exec" {
-    when    = destroy
-    command = "python ./pop-dockerfile.py --repository ${self.triggers.repository_name} --region ${self.triggers.region} --profile ${self.triggers.profile} --image_tag all"
+    when       = destroy
+    on_failure = fail
+    command    = "python ./pop-dockerfile.py --repository ${self.triggers.repository_name} --region ${self.triggers.region} --profile ${self.triggers.profile} --image_tag all"
   }
 
   triggers = {
