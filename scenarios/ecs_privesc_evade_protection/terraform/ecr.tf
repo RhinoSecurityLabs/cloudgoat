f9c56e371abc9a6a388666132facd503665d4db8
ysu <siuy2685@gmail.com>
2023-12-24 12:57:06 +0900

    change command `python` to `python3` in local-exec

--- scenarios/ecs_privesc_evade_protection/terraform/ecr.tf
+++ scenarios/ecs_privesc_evade_protection/terraform/ecr.tf
@@ -8,14 +8,14 @@ resource "null_resource" "docker_image" {
   provisioner "local-exec" {
     when       = create
     on_failure = fail
-    command    = "python ./push-dockerfile.py --dockerfile_path ../assets/ssrf-web/ --repository ${aws_ecr_repository.repository.name} --region ${var.region} --profile ${var.profile} --image_tag latest"
+    command    = "python3 ./push-dockerfile.py --dockerfile_path ../assets/ssrf-web/ --repository ${aws_ecr_repository.repository.name} --region ${var.region} --profile ${var.profile} --image_tag latest"
   }
 
   # Pop Docker images when the scenario be destroyed
   provisioner "local-exec" {
     when       = destroy
     on_failure = fail
-    command    = "python ./pop-dockerfile.py --repository ${self.triggers.repository_name} --region ${self.triggers.region} --profile ${self.triggers.profile} --image_tag all"
+    command    = "python3 ./pop-dockerfile.py --repository ${self.triggers.repository_name} --region ${self.triggers.region} --profile ${self.triggers.profile} --image_tag all"
   }
 
   triggers = {
