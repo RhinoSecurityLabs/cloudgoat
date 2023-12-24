e99042f4a1021a6a12d2f97e85b3bfc6b5d67856
ysu <siuy2685@gmail.com>
2023-12-24 13:00:31 +0900

    add `depends on for s3 policy` in cloudtrail.tf

--- scenarios/ecs_privesc_evade_protection/terraform/cloudtrail.tf
+++ scenarios/ecs_privesc_evade_protection/terraform/cloudtrail.tf
@@ -3,4 +3,6 @@ resource "aws_cloudtrail" "cloudtrail" {
   name           = "cg-cloudtrail-${var.cgid}"
   s3_bucket_name = aws_s3_bucket.cloudtrail_bucket.id
   enable_logging = true
+
+  depends_on = [aws_s3_bucket_policy.trail_bucket_policy]
 }
