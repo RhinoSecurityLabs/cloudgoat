f72fe12ea09b6da762dcd898fffb40cc3e21cfd1
Andrew Aiken <andrew.aiken@machinemetrics.com>
2024-04-04 13:00:04 -0400

    Refactor lambda privesc

--- scenarios/lambda_privesc/terraform/provider.tf
+++ scenarios/lambda_privesc/terraform/provider.tf
@@ -1,4 +1,22 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+  }
+}
+
 provider "aws" {
   profile = var.profile
-  region = var.region
+  region  = var.region
+
+  default_tags {
+    tags = {
+      Stack    = var.stack-name
+      Scenario = var.scenario-name
+    }
+  }
 }
