e356252a4b4c331c83a571eeae476640905b078b
Andrew Aiken <andrew.aiken@machinemetrics.com>
2024-08-04 16:03:32 -0400

    Update SSRF scenario, webserver and terraform code

--- scenarios/ec2_ssrf/terraform/s3.tf
+++ scenarios/ec2_ssrf/terraform/s3.tf
@@ -1,29 +1,19 @@
-#Secret S3 Bucket
-locals {
-  # Ensure the bucket suffix doesn't contain invalid characters
-  # "Bucket names can consist only of lowercase letters, numbers, dots (.), and hyphens (-)."
-  # (per https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html) 
-  bucket_suffix = replace(var.cgid, "/[^a-z0-9-.]/", "-")
-}
-
-resource "aws_s3_bucket" "cg-secret-s3-bucket" {
-  bucket = "cg-secret-s3-bucket-${local.bucket_suffix}"
+resource "aws_s3_bucket" "secret_bucket" {
+  bucket        = "cg-secret-s3-bucket-${replace(var.cgid, "/[^a-z0-9-.]/", "-")}"
   force_destroy = true
+
   tags = {
-      Name = "cg-secret-s3-bucket-${local.bucket_suffix}"
-      Description = "CloudGoat ${var.cgid} S3 Bucket used for storing a secret"
-      Stack = "${var.stack-name}"
-      Scenario = "${var.scenario-name}"
+    Description = "CloudGoat ${var.cgid} S3 Bucket used for storing a secret"
   }
 }
 
-resource "aws_s3_object" "cg-shepards-credentials" {
-  bucket = "${aws_s3_bucket.cg-secret-s3-bucket.id}"
-  key = "admin-user.txt"
-  source = "../assets/admin-user.txt"
-  tags = {
-    Name = "cg-shepards-credentials-${var.cgid}"
-    Stack = "${var.stack-name}"
-    Scenario = "${var.scenario-name}"
-  }
+resource "aws_s3_object" "shepards_credentials" {
+  bucket  = aws_s3_bucket.secret_bucket.id
+  key     = "/aws/credentials"
+  content = <<-CONTENT
+    [default]
+    aws_access_key_id = ${aws_iam_access_key.shepard.id}
+    aws_secret_access_key = ${aws_iam_access_key.shepard.secret}
+    region = ${var.region}
+  CONTENT
 }
