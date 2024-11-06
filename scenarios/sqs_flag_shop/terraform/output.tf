41fa606ce156cf022f1f55ab3f4eb269599ac14a
Andrew Aiken <andrew.aiken@machinemetrics.com>
2024-11-05 20:47:24 -0500

    Update SQS Flag Shop Terraform

--- scenarios/sqs_flag_shop/terraform/output.tf
+++ scenarios/sqs_flag_shop/terraform/output.tf
@@ -1,12 +1,12 @@
 output "cloudgoat_output_sqsuser_access_key_id" {
-  value = aws_iam_access_key.cg-sqs-user_access_key.id
+  value = aws_iam_access_key.sqs.id
 }
 
 output "cloudgoat_output_sqsuser_secret_key" {
-  value     = aws_iam_access_key.cg-sqs-user_access_key.secret
+  value     = aws_iam_access_key.sqs.secret
   sensitive = true
 }
 
-output "cg_web_site_ip" {
-  value = "${aws_instance.cg_flag_shop_server.public_ip}:5000"
+output "web_site_ip" {
+  value = "http://${aws_instance.flag_shop_server.public_ip}:5000"
 }
