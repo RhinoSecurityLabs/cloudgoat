a2082b369972ee6db2f72e8e0b69972c91a1065a
Andrew <61951628+andrew-aiken@users.noreply.github.com>
2025-01-15 16:47:46 -0500

    Remove unused provider
    Copied this over from an another scenario

--- scenarios/scenario_template/terraform/provider.tf
+++ scenarios/scenario_template/terraform/provider.tf
@@ -5,11 +5,6 @@ terraform {
   required_version = ">= 1.5"
 
   required_providers {
-    archive = {
-      source  = "hashicorp/archive"
-      version = ">= 2.6"
-    }
-
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.74.0"
