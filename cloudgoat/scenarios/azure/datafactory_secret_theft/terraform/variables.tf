bf125d7931a1bb2ea2d8800205b1a62f0453f392
DaveYesland <david.yesland@rhinosecuritylabs.com>
2025-03-21 08:52:49 -0700

    Remove unused whitelist variable

--- cloudgoat/scenarios/azure/datafactory_secret_theft/terraform/variables.tf
+++ cloudgoat/scenarios/azure/datafactory_secret_theft/terraform/variables.tf
@@ -1,9 +1,3 @@
-# variable "cg_whitelist" {
-#   description = "User's public IP address(es)"
-#   type        = list(string)
-#   default     = []
-# }
-
 variable "cgid" {
   description = "CGID variable for unique naming between scenarios"
   type        = string
