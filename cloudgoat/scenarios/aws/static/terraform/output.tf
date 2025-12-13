6ae21d13b14b8bccf78a3dc2b5822adf71246b6a
TeneBrae93 <tyramsbey@gmail.com>
2025-12-12 21:37:17 -0600

    Removed spaces from output

--- cloudgoat/scenarios/aws/static/terraform/output.tf
+++ cloudgoat/scenarios/aws/static/terraform/output.tf
@@ -1,6 +1,6 @@
 # outputs.tf
 
-output "Website In Scope" {
+output "Website_In_Scope" {
   description = "The URL of the target web server."
   value       = "http://${aws_instance.instance.public_dns}"
 }
