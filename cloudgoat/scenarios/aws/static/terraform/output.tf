f98cebd0d2419b223bf404416ea2e60b5714ae2d
TeneBrae93 <tyramsbey@gmail.com>
2025-12-13 21:29:49 -0600

    Updated output variable

--- cloudgoat/scenarios/aws/static/terraform/output.tf
+++ cloudgoat/scenarios/aws/static/terraform/output.tf
@@ -1,6 +1,6 @@
 # outputs.tf
 
-output "website_in_scope" {
+output "Website_In_Scope_START_HERE" {
   description = "The URL of the target web server."
   # We reference the EIP now, which is guaranteed to exist
   value       = "http://${aws_eip.web_ip.public_ip}"
